f0d2f69838cbb0ac4c68a1ab30afbc0adecf5246
Robert Stolz <robert.stolz87@gmail.com>
2025-05-13 19:02:58 +0200

    endpoint no-image and tests

--- backend/src/main/java/ropold/backend/controller/QuestionController.java
+++ backend/src/main/java/ropold/backend/controller/QuestionController.java
@@ -78,6 +78,24 @@ public class QuestionController {
         );
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/no-image")
+    public QuestionModel addQuestionWithNoImage(@RequestBody QuestionModelDto questionModelDto){
+        return questionService.addQuestion(
+                new QuestionModel(
+                        null,
+                        questionModelDto.title(),
+                        questionModelDto.difficultyEnum(),
+                        questionModelDto.categoryEnum(),
+                        questionModelDto.questionText(),
+                        questionModelDto.options(),
+                        questionModelDto.answerExplanation(),
+                        questionModelDto.isActive(),
+                        questionModelDto.githubId(),
+                        null
+                ));
+    }
+
     @PutMapping("/{id}")
     public QuestionModel updateQuestion(
             @PathVariable String id,
